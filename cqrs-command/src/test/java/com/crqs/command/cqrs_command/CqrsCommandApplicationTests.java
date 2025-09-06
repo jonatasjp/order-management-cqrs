accddb801988fb3c908f926e2d2e4e733e133d9f
Jonatas Luis <jonatasluis.unipe@gmail.com>
2025-09-06 12:35:29 -0300

    Update CqrsCommandApplicationTests.java

--- cqrs-command/src/test/java/com/crqs/command/cqrs_command/CqrsCommandApplicationTests.java
+++ cqrs-command/src/test/java/com/crqs/command/cqrs_command/CqrsCommandApplicationTests.java
@@ -2,6 +2,7 @@ package com.crqs.command.cqrs_command;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
